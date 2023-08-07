f200d8e7600af5698de5ce4fa68f7d3a7e33dbc7
arunacharya1603 <arun2k20it@csjmu.ac.in>
2023-08-07 18:27:34 +0530

    urls updated

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/curd")
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.mctn26u.mongodb.net/?retryWrites=true&w=majority")
 
 app.get('/', (req, res) => {
     UserModel.find({})
